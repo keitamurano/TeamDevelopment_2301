746d44444ccf2908b48c75a787ca55a92fd88d5e
keitamurano <7banters7@gmail.com>
2023-01-10 19:04:39 +0900

    プロジェクトのコミット

---
+++ src/main/java/com/example/demo/TeamDebelopment2301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDebelopment2301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDebelopment2301Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/TeamDebelopment2301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDebelopment2301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
