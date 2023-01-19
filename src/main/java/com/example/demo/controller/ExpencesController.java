ce62a0c550a71881a666997b5fe723eb0a36bcf1
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-20 01:34:59 +0900

    経費申請画面、動作確認未実施（狩野）

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -1,35 +1,35 @@
-package com.example.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class ExpencesControoller {
-
-
-	//トップページ（経費申請画面）を表示
-   //経費申請のボタンのformから
-	@GetMapping("/top")
-	public String top(Model model) {
-		return "expences";
-	}
-
-    //	戻るボタンを押すとマイページ画面に遷移
-	@GetMapping("/back")
-	public String back(Model model) {
-		return "Mypage";
-	}
-
-    //	申請ボタンを押すと経費一覧画面に遷移
-	@PostMapping("")
-	public String regist(@ModelAttribute expencesForm expencesForm,
-						Model model) {
-			expencesEntity expencesEntity = expencesEntity.createExpences(expencesForm);
-			model.addAttribute("expencesEntity", expencesEntity);
-			return "expenceslist";
-		}
-
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class ExpencesControoller {
+//
+//
+//	//トップページ（経費申請画面）を表示
+//   //経費申請のボタンのformから
+//	@GetMapping("/top")
+//	public String top(Model model) {
+//		return "expences";
+//	}
+//
+//    //	戻るボタンを押すとマイページ画面に遷移
+//	@GetMapping("/back")
+//	public String back(Model model) {
+//		return "Mypage";
+//	}
+//
+//    //	申請ボタンを押すと経費一覧画面に遷移
+//	@PostMapping("")
+//	public String regist(@ModelAttribute expencesForm expencesForm,
+//						Model model) {
+//			expencesEntity expencesEntity = expencesEntity.createExpences(expencesForm);
+//			model.addAttribute("expencesEntity", expencesEntity);
+//			return "expenceslist";
+//		}
+//
+//}
