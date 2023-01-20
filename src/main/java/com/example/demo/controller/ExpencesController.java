9949d8816a1e4ce6c5e0ba739188f5ea73631ea0
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-21 00:12:34 +0900

    経費申請、経費一覧のフロント修正（Thymeleafの反映等）、コントローラーの作成、フォーム作成（狩野）

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -1,35 +1,45 @@
-//package com.example.demo.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//public class ExpencesControoller {
-//
-//
-//	//トップページ（経費申請画面）を表示
-//   //経費申請のボタンのformから
-//	@GetMapping("/top")
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExpencesController {
+
+
+	//トップページ（経費申請画面）を表示
+   //マイページ画面の「経費申請」ボタンから
+	@GetMapping("/top")
 //	public String top(Model model) {
-//		return "expences";
-//	}
-//
-//    //	戻るボタンを押すとマイページ画面に遷移
-//	@GetMapping("/back")
+	public String top() {
+			return "expences";
+		}
+
+
+    //	経費申請画面で「戻る」ボタンを押すとマイページ画面に遷移
+	@GetMapping("/back")
 //	public String back(Model model) {
-//		return "Mypage";
-//	}
-//
-//    //	申請ボタンを押すと経費一覧画面に遷移
-//	@PostMapping("")
-//	public String regist(@ModelAttribute expencesForm expencesForm,
-//						Model model) {
-//			expencesEntity expencesEntity = expencesEntity.createExpences(expencesForm);
+	public String back() {
+		return "Mypage";
+	}
+
+    //	申請ボタンを押すと経費一覧画面に遷移
+//	@PostMapping("/")
+//	public String regist(@ModelAttribute ExpencesForm expencesForm,
+//						      Model model) {
+//			ExpencesForm expencesForm = expencesService.createExpences(expencesForm);
 //			model.addAttribute("expencesEntity", expencesEntity);
 //			return "expenceslist";
 //		}
-//
-//}
+
+
+    //	経費一覧画面で「戻る」ボタンを押すとマイページ画面に遷移
+	@GetMapping("/mypageBack")
+//	public String back(Model model) {
+	public String mypageBack() {
+		return "Mypage";
+	}
+
+
+
+}
---
+++ src/main/java/com/example/demo/form/ExpencesForm.java
@@ -0,0 +1,19 @@
+package com.example.demo.form;
+
+import lombok.Data;
+@Data
+public class ExpencesForm {
+
+
+
+		private Integer userId;
+
+//		private Date applicationDate;
+
+		private String expencesItem;
+
+		private Integer amountMoney;
+
+		private String remarks;
+	}
+
