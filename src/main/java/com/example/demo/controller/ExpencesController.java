d87f1b075ee031fbe4e4da928d35608e5b153e67
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-22 14:16:31 +0900

    経費申請、経費一覧（コントローラー修正）（狩野）

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.form.ExpencesForm;
 
 @Controller
 public class ExpencesController {
@@ -17,11 +22,26 @@ public class ExpencesController {
 
 
     //	経費申請画面で「戻る」ボタンを押すとマイページ画面に遷移
-	@GetMapping("/back")
+//	@GetMapping("/back")
 //	public String back(Model model) {
-	public String back() {
-		return "Mypage";
+//	public String back() {
+//		return "Mypage";
+//	}
+
+
+    @RequestMapping(value = "confirm", params = "back", method = RequestMethod.POST)
+    public String back() {
+		   return "Mypage";
+    }
+
+    //	経費申請画面で「申請」ボタンを押すと経費一覧画面に遷移
+	@GetMapping("/regist")
+	public String regist(Model model,ExpencesForm expencesForm) {
+    //Modelへの追加
+       model.addAttribute("expences",expencesForm);
+		   return "expenceslist";
 	}
+
 
     //	申請ボタンを押すと経費一覧画面に遷移
 //	@PostMapping("/")
--- src/main/java/com/example/demo/form/ExpencesForm.java
+++ src/main/java/com/example/demo/form/ExpencesForm.java
@@ -1,17 +1,19 @@
 package com.example.demo.form;
 
+import com.sun.istack.NotNull;
+
 import lombok.Data;
 @Data
 public class ExpencesForm {
 
 
-
+       @NotNull
 		private Integer userId;
 
 //		private Date applicationDate;
-
+       @NotNull
 		private String expencesItem;
-
+       @NotNull
 		private Integer amountMoney;
 
 		private String remarks;
