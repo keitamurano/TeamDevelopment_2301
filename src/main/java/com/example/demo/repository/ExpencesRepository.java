ee2dd7d019f07391f9b61b832c92a6e8f5e228b6
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-22 16:35:17 +0900

    経費申請（リポジトリ）狩野

--- src/main/java/com/example/demo/repository/ExpencesRepository.java
+++ src/main/java/com/example/demo/repository/ExpencesRepository.java
@@ -1,12 +1,17 @@
 package com.example.demo.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.PaidEntity;
+
+import schoo.entity.Event;
 /**
  * ユーザー情報 Repository
  */
 @Repository
-public interface ExpencesRepository extends JpaRepository<ExpencesForm, integer> {
-	public List<ExpencesForm> findAll()
+public interface ExpencesRepository extends JpaRepository<ExpencesEntity, integer> {
+
+	public List<ExpencesEntity> findAll();
 }
