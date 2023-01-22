0829bb9be7a61f0c0265688eaeffcddd8276119d
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-22 16:17:55 +0900

    経費申請（フォーム）狩野

--- src/main/java/com/example/demo/form/ExpencesForm.java
+++ src/main/java/com/example/demo/form/ExpencesForm.java
@@ -1,17 +1,16 @@
 package com.example.demo.form;
 
-import com.sun.istack.NotNull;
 import lombok.Data;
 @Data
 public class ExpencesForm {
 
-       @NotNull
+
 		private Integer userId;
-       @NotNull
+
 //		private Date applicationDate;
-       @NotNull
+
 		private String expencesItem;
-       @NotNull
+
 		private Integer amountMoney;
 
 		private String remarks;
