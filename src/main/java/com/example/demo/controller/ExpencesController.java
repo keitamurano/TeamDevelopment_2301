b60a84e96b771d9955e8101ffea8677d4c0b88cc
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-24 23:22:06 +0900

    経費申請、経費一覧（controller）狩野

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.dto.ExpencesListParam;
 import com.example.demo.form.ExpencesForm;
+import com.example.demo.service.ExpencesService;
 
 @Controller
 public class ExpencesController {
+
+
+
+	  /**
+	   * ユーザー情報 Service
+	   */
+	  @Autowired
+	  private ExpencesService expencesService;
+
 
 	//トップページ（経費申請画面）を表示
 	//マイページ画面の「経費申請」ボタンから
@@ -23,6 +34,8 @@ public class ExpencesController {
 //        model.addAttribute("demoForm", expencesForm);
 //        return "expenceslist";
 //    }
+
+//	経費申請、経費一覧ともに「戻る」ボタンでマイページ画面に遷移
 	@GetMapping("/Mypage")
 	public String back() {
 		return "/Mypage";
@@ -58,13 +71,25 @@ public class ExpencesController {
 //		return "/Mypage";
 //	}
 
+//	   ユーザー情報一覧画面を表示
+//	    @param model Model
+//	    @return ユーザー情報一覧画面
+
+	  @GetMapping(value = "/user/list")
+	  public String displayList(Model model) {
+		  ExpencesListParam expencesListParam = expencesService.searchAll();
+	    model.addAttribute("expences", expencesListParam);
+	    return "user/list";
+	  }
+
+
 	//	経費申請画面で「申請」ボタンを押すと経費一覧画面に遷移
-	@GetMapping("/confirm")
-	public String regist(@ModelAttribute ExpencesForm expencesForm, Model model) {
-		//Modelへの追加
-		model.addAttribute("expenceslist.", expencesForm);
-		return "/expenceslist";
-	}
+//	@GetMapping("/confirm")
+//	public String regist(@ModelAttribute ExpencesForm expencesForm, Model model) {
+//		//Modelへの追加
+//		model.addAttribute("expenceslist.", expencesForm);
+//		return "/expenceslist";
+//	}
 
 	//	申請ボタンを押すと経費一覧画面に遷移
 	//	@PostMapping("/")
