ba1eb1a80c038b17574fce070e5038ad257de6af
加瀬 結夏 <kase.20220801@gmail.com>
2023-01-24 23:46:21 +0900

    Newuser.java
    Newuser.java　コントローラー作成
    初期初期表示完了

---
+++ src/main/java/com/example/demo/controller/Newuser.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Newuser {
+
+	    @RequestMapping("/user")
+	    public String user() {
+//	        String username = httpServletRequest.getRemoteUser();
+////	        User user = userRepository.getOne(username);
+////	        modelMap.addAttribute("user", user);
+	        return "/Newuser";
+	    }
+
+	}
+
+	//1行目: 引数にHttpServletRequestを指定することによってそのオブジェクトが取得できる。
+	//2行目: getRemoteUserメソッドでユーザ名を取得する。
+	//3行目: getOneメソッドは、JpaRepositoryに最初から用意されているメソッドで、IDからインスタンスを取得することができる。
+	//4行目: addAttributeメソッドで、userをビューに送る。String以外のオブジェクトでもビューに送ることができる。
+
