3d1a7033a54dda4624d8f2a4010011879dd8f670
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-27 00:26:18 +0900

    経費申請、経費一覧（controller）狩野

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.demo.dto.ExpencesListParam;
 import com.example.demo.form.ExpencesForm;
@@ -41,66 +42,44 @@ public class ExpencesController {
 		return "/Mypage";
 	}
 
+
+
+
 //	@GetMapping("/expenceslist")
-//	public String application() {
+//	public String application(Model mode) {
+//	    List<ExpencesForm> list = new ArrayList<ExpencesForm>();
+//	    list.add(new ExpencesForm());
 //		return "/expenceslist";
 //	}
 
 
-	@GetMapping("/top2")
-	public String top2() {
-		return "/expenceslist";
-	}
-
-
-	//	経費申請画面で「戻る」ボタンを押すとマイページ画面に遷移
-	//	@GetMapping("/back")
-	//	public String back(Model model) {
-	//	public String back() {
-	//		return "Mypage";
-	//	}
-
-
-	//経費申請画面「戻る」ボタンを押すとマイページ画面に遷移
-//	@RequestMapping(value = "@{/confirm}", params = "back", method = RequestMethod.POST)
-//	public String back() {
-//		return "/Mypage";
-//	}
-
-	//経費一覧画面「戻る」ボタンを押すとマイページ画面に遷移
-//	@RequestMapping(value = "@{/confirm2}", params = "back", method = RequestMethod.POST)
-//	public String back2() {
-//		return "/Mypage";
-//	}
 
 //	   ユーザー情報一覧画面を表示
 //	    @param model Model
 //	    @return ユーザー情報一覧画面
-
-	  @GetMapping(value = "/user/list")
+	  @GetMapping("expenceslist")
 	  public String displayList(Model model) {
-		  ExpencesListParam expencesListParam = expencesService.searchAll();
+		ExpencesListParam expencesListParam = expencesService.searchAll();
 	    model.addAttribute("expences", expencesListParam);
-	    return "user/list";
+	    return "expenceslist";
 	  }
 
+//	  @GetMapping(value = "expenceslist")
+//	  public String displayList(Model model) {
+//		ExpencesListParam expencesListParam = expencesService.searchAll();
+//	    model.addAttribute("expences", expencesListParam);
+//	    return "expenceslist";
+//	  }
 
-	//	経費申請画面で「申請」ボタンを押すと経費一覧画面に遷移
-//	@GetMapping("/confirm")
-//	public String regist(@ModelAttribute ExpencesForm expencesForm, Model model) {
-//		//Modelへの追加
-//		model.addAttribute("expenceslist.", expencesForm);
-//		return "/expenceslist";
-//	}
 
-	//	申請ボタンを押すと経費一覧画面に遷移
-	//	@PostMapping("/")
-	//	public String regist(@ModelAttribute ExpencesForm expencesForm,
-	//						      Model model) {
-	//			ExpencesForm expencesForm = expencesService.createExpences(expencesForm);
-	//			model.addAttribute("expencesEntity", expencesEntity);
-	//			return "expenceslist";
-	//		}
+//		経費申請画面で「申請」ボタンを押すと経費一覧画面に遷移
+	@GetMapping("/confirm")
+	public String regist(@ModelAttribute ExpencesForm expencesForm, Model model) {
+		//Modelへの追加
+		model.addAttribute("expenceslist.", expencesForm);
+		return "/expenceslist";
+	}
+
 
 	//	経費一覧画面で「戻る」ボタンを押すとマイページ画面に遷移
 	@GetMapping("/mypageBack")
