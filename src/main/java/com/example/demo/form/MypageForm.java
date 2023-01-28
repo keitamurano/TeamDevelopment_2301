d59f5340ddf30c0efbf194b5f674773497ab2448
amie15 <109030666+amie15@users.noreply.github.com>
2023-01-28 17:24:59 +0900

    kikuchi.

---
+++ src/main/java/com/example/demo/form/MypageForm.java
@@ -0,0 +1,23 @@
+/**
+package com.example.demo.form;
+
+public class MypageForm {
+	private String name;
+	private String userID;
+	
+	
+	public MypageForm() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String genUserID() {
+		return userID;
+	}
+}
+
+
+**/
--- src/main/java/com/example/demo/service/PaidService.java
+++ src/main/java/com/example/demo/service/PaidService.java
@@ -1,5 +1,47 @@
+
 package com.example.demo.service;
 
-public class PaidService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.PaidEntity;
+import com.example.demo.repository.PaidRepository;
+/**
+@Service
+		public class PaidService {
+
+
+    @Autowired
+    private PaidRepository paidRepository;
+    
+
+    public PaidEntity search(PaidForm paidForm) {
+        return paidRepository.search(paidForm);
+    }
+}
+
+**/
+
+//----------------------------------------------------
+
+
+/**
+ * ユーザー情報 Service
+ */
+@Service
+public class PaidService {
+  /**
+   * ユーザー情報 Repository
+   */
+  @Autowired
+  private PaidRepository paidRepository;
+  /**
+   * ユーザー情報 全検索
+   * @return 検索結果
+   */
+  public List<PaidEntity> searchAll() {
+    return paidRepository.findAll();
+  }
 }
