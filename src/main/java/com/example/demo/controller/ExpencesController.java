630b8b78d20b5f81133d234638b9564b40e86cb5
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-01-30 00:31:33 +0900

    経費申請、経費一覧（ controller）狩野

--- src/main/java/com/example/demo/controller/ExpencesController.java
+++ src/main/java/com/example/demo/controller/ExpencesController.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.dto.ExpencesListParam;
 import com.example.demo.entity.ExpencesEntity;
+import com.example.demo.form.ExpencesForm;
 import com.example.demo.service.ExpencesService;
 
 @Controller
@@ -26,13 +25,32 @@ public class ExpencesController {
 	  private ExpencesService expencesService;
 
 //OK------------------------------------------------------------------------------------------------------------
+//		   * 経費一覧画面を表示
+//		   * @param model Model
+//		   * @return 経費一覧画面
+//	    @RequestMapping(value = "/expenceslist", method = RequestMethod.GET)
+	  @GetMapping("/expenceslist")
+//	  @PostMapping("/expenceslist")
+	  public String displayList(Model model) {
+		// serviceを使って、申請の一覧をDBから取得する
+		  List<ExpencesEntity> expenceslist = expencesService.getExpencesAll();
+//		    List<Prefectures> prefectures = expencesService.getPrefecturesAll();
+		// modelに経費の一覧を設定して、画面に渡す
+		  model.addAttribute("expenceslist", expenceslist);
+//		    model.addAttribute("prefecturesList", prefectures);
+		  return "/expenceslist";
+	  }
+
+//OK------------------------------------------------------------------------------------------------------------
 //経費申請画面で空のフォームを用意する(ここでプルダウンの値が渡されている)
 //	  経費申請画面を表示
-	  @RequestMapping(value = "/expences", method = RequestMethod.GET)
+//	  @RequestMapping(value = "/expences", method = RequestMethod.GET)
+	  @GetMapping("/expences")
+//	  @PostMapping("/expences")
 	  public String top(Model model) {
 //			List<Prefectures> prefecturesList = expencesService.getPrefecturesAll();
 //			List<ExpencesEntity> expencesEntity = expencesService.getExpencesAll();
-		  model.addAttribute("expencesListParam",new ExpencesListParam());
+		  model.addAttribute("expencesForm",new ExpencesForm());
 //			model.addAttribute("expencesList", expencesEntity);
 //			model.addAttribute("prefecturesList", prefecturesList);
 		  return "/expences";
@@ -40,18 +58,7 @@ public class ExpencesController {
 
 
 
-//OK------------------------------------------------------------------------------------------------------------
-//		   * 経費一覧画面を表示
-//		   * @param model Model
-//		   * @return 経費一覧画面
-	    @RequestMapping(value = "/expenceslist", method = RequestMethod.POST)
-	  public String displayList(Model model) {
-		  List<ExpencesEntity> expenceslist = expencesService.getExpencesAll();
-//		    List<Prefectures> prefectures = expencesService.getPrefecturesAll();
-		  model.addAttribute("expenceslist", expenceslist);
-//		    model.addAttribute("prefecturesList", prefectures);
-		  return "/expenceslist";
-	  }
+
 
 
 		    /**
@@ -60,12 +67,14 @@ public class ExpencesController {
 		     * @param model Model
 		     * @return ユーザー情報一覧画面
 		     */
-		    @RequestMapping(value = "/create", method = RequestMethod.POST)
-		    public String create( @ModelAttribute ExpencesListParam expencesListParam, Model model) {
-//		    public String create(@ModelAttribute ExpencesForm expencesForm) {
-		        // ユーザー情報の登録
-		        expencesService.create(expencesListParam);
+//		    @RequestMapping(value = "/expenceslist", method = RequestMethod.POST)
+	        @PostMapping("/expences")
+//	        @GetMapping("/expences")
+		    public String create( @ModelAttribute ExpencesForm expencesForm, Model model) {
+		        // 経費申請の登録
+		        expencesService.insert(expencesForm);
 //		        expencesService.create(expencesForm);
+		     // 経費一覧画面にリダイレクト
 		        return "redirect:/expenceslist";
 		    }
 
