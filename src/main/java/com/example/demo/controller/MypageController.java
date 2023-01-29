e74f2184f0d52284be3242f80ef506af5b010860
amie15 <109030666+amie15@users.noreply.github.com>
2023-01-29 14:18:14 +0900

    。

--- src/main/java/com/example/demo/controller/MypageController.java
+++ src/main/java/com/example/demo/controller/MypageController.java
@@ -6,6 +6,71 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+
+public class MypageController {
+
+	
+	 @GetMapping(value ="/mypage")
+	  public String mypage(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+	    return "Mypage";
+	  }
+	 
+		 
+	  @PostMapping("/edit")
+	  public String postEdit(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "edit";
+	  }
+	  
+	  @PostMapping("/attendance"/**path="/mypage", params = "attendance-button"**/)
+	  public String postAttendance(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "attendance";
+	  }
+	  
+	  @PostMapping("/leavingwork"/**path="/mypage", params = "leavingwork-button"**/)
+	  public String postLeavingwork(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "leavingwork";
+	  }
+	  
+	  @GetMapping("/create" /** path="/mypage", params = "create-button"**/)
+	  public String postCreate(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "create";
+	  }
+	  
+	  @GetMapping("/create2" /**path="/mypage", params = "create2-button"**/)
+	  public String postCreate2(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "create2";
+	  }
+	  
+	  @PostMapping("/top" /**path="/mypage", params = "expences-button"**/)
+	  public String postExpences(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "expences";
+	  }
+	  
+	  @PostMapping("/top2" /**path="/mypage", params = "expanceslist-button"**/)
+	  public String postExpanceslist(/**@RequestParam String name, String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "expenceslist";
+	  }
+	  
+	  @PostMapping("/paid/list" /**path="/mypage", params = "paidlist-button"**/)
+	  public String postPaidlist(/**@RequestParam String userID, **/Model model) {
+		// model.addAllAttributes("name",name,"userID",userID);
+		  return "Paid";
+	  }
+	
+}
+
+
+
+/**
 
 @Controller
 
@@ -53,10 +118,12 @@ public class MypageController {
 		  return "expenceslist";
 	  }
 	  
-	  @GetMapping(path="/mypage", params = "paidlist-button")
+	  @PostMapping(path="/mypage", params = "paidlist-button")
 	  public String postPaidlist() {
 		  return "Paid";
 	  }
 	
 }
+
+**/
 
