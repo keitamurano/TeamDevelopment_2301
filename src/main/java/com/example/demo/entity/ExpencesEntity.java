b4261a9003ed225b5da833b3f9e8275e9a6c6068
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-02-03 00:14:42 +0900

    経費申請（フロント）狩野

--- src/main/java/com/example/demo/entity/ExpencesEntity.java
+++ src/main/java/com/example/demo/entity/ExpencesEntity.java
@@ -3,22 +3,19 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
 
-@Data
 @Entity
+@Data
 @Table(name="expences")
 public class ExpencesEntity{
-//public class ExpencesEntity implements Serializable {
 
     // 予約データの主キーはユーザーID
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
     private Integer userId;
 
