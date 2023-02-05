e29e75b934ae499d4fd1b75b0f24533ba9aa1492
tolbou <ttorubou@gmail.com>
2023-02-05 19:01:19 +0900

    ログイン機能 V1.0

--- src/main/java/com/example/demo/TeamDebelopment2301Application.java
+++ src/main/java/com/example/demo/TeamDebelopment2301Application.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.demo.entity")
+@EnableJpaRepositories("com.example.demo.repository")
 public class TeamDebelopment2301Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,38 +1,92 @@
-//package com.example.demo.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//	
-//	
-//	/**セキュリティの対象外を設定*/
-//	@Override
-//	public void configure(WebSecurity web)throws Exception {
-//		//セキュリティを適用しない
-//		web
-//		 .ignoring()
-//		 	.antMatchers("/webjars")
-//		 	.antMatchers("/css/**")
-//		 	.antMatchers("/js/**")
-//		 	.antMatchers("/h2-console/**");
-//	}
-//	
-//	@Override
-//	protected void configure(HttpSecurity http)throws Exception {
-//		
-//		//ログイン不要ページの設定
-//		http
-//			.authorizeRequests()
-//				.antMatchers("/login1").permitAll()//直リンクOK
-//				.anyRequest().authenticated();//それ以外はリンクNG
-//				
-//		//CSRF対策を無効に設定(一時的)
-//		http.csrf().disable();		
-//	}
-//}
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.service.UserDetailsServiceImpl;
+
+
+
+/**
+ * SpringSecurityを利用するための設定クラス
+ * ログイン処理でのパラメータ、画面遷移や認証処理でのデータアクセス先を設定する
+ * @author aoi
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	//フォームの値と比較するDBから取得したパスワードは暗号化されているのでフォームの値も暗号化するために利用
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
+	/**
+	 * 認可設定を無視するリクエストを設定
+	 * 静的リソース(image,javascript,css)を認可処理の対象から除外する
+	 */
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                            "/images/**",
+                            "/css/**",
+                            "/javascript/**"
+                            );
+    }
+	
+	/**
+	 * 認証・認可の情報を設定する
+	 * 画面遷移のURL・パラメータを取得するname属性の値を設定
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		    .authorizeRequests()
+		        .anyRequest().authenticated()
+		        .and()
+		    .formLogin()
+		        .loginPage("/login") //ログインページはコントローラを経由しないのでViewNameとの紐付けが必要
+		        .loginProcessingUrl("/sign_in") //フォームのSubmitURL、このURLへリクエストが送られると認証処理が実行される
+		        .usernameParameter("username") //リクエストパラメータのname属性を明示
+		        .passwordParameter("password")
+		        .successForwardUrl("/hello")
+		        .failureUrl("/login?error")
+		        .permitAll()
+		        .and()
+		    .logout()
+		        .logoutUrl("/logout")
+		        .logoutSuccessUrl("/login?logout")
+		        .permitAll();
+	}
+	
+	/**
+	 * 認証時に利用するデータソースを定義する設定メソッド
+	 * ここではDBから取得したユーザ情報をuserDetailsServiceへセットすることで認証時の比較情報としている
+	 * @param auth
+	 * @throws Exception
+	 */
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		/*
+		auth
+		    .inMemoryAuthentication()
+		        .withUser("user").password("{noop}password").roles("USER");
+		*/
+	}
+
+}
