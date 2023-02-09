4bdb6ab3b484ee849bc580debb654103f32cfb67
tolbou <ttorubou@gmail.com>
2023-02-10 01:02:17 +0900

    ログインしいるユーザー名の取得

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -19,6 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
 	 */
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/mypage").setViewName("Mypage");
 	}
 
 }
