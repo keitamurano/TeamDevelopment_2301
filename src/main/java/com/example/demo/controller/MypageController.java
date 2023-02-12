cfe25def7f7eef7f990aad67fc77598919316a7c
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-02-12 20:21:52 +0900

    マイページ（顔文字遷移先編集）狩野

--- src/main/java/com/example/demo/controller/MypageController.java
+++ src/main/java/com/example/demo/controller/MypageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MypageController {
 
 
-	
+
 	 @GetMapping(value ="/mypage")
 	  public String mypage(/**@RequestParam String name, String userID, **/Model model) {
 //		// model.addAllAttributes("name",name,"userID",userID);
@@ -27,56 +27,56 @@ public class MypageController {
 		model.addAttribute("user", userName);
 		return "Mypage";
 	  }
-	 
-		 
+
+
 	  @PostMapping("/edit")
 	  public String postEdit(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "edit";
 	  }
-	  
+
 	  @PostMapping("/attendance"/**path="/mypage", params = "attendance-button"**/)
 	  public String postAttendance(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "attendance";
 	  }
-	  
+
 	  @PostMapping("/leavingwork"/**path="/mypage", params = "leavingwork-button"**/)
 	  public String postLeavingwork(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "leavingwork";
 	  }
-	  
+
 	  @GetMapping("/kintaiichiran" /** path="/mypage", params = "create-button"**/)
 	  public String postCreate(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "kintaiichiran";
 	  }
-	  
-	  @GetMapping("/edit" /**path="/mypage", params = "create2-button"**/)
+
+	  @GetMapping("/Newuser" /**path="/mypage", params = "create2-button"**/)
 	  public String postCreate2(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
-		  return "edit";
+		  return "Newuser";
 	  }
-	  
+
 	  @PostMapping("/top" /**path="/mypage", params = "expences-button"**/)
 	  public String postExpences(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "expences";
 	  }
-	  
+
 	  @PostMapping("/top2" /**path="/mypage", params = "expanceslist-button"**/)
 	  public String postExpanceslist(/**@RequestParam String name, String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "expenceslist";
 	  }
-	  
+
 	  @PostMapping("/paid/list" /**path="/mypage", params = "paidlist-button"**/)
 	  public String postPaidlist(/**@RequestParam String userID, **/Model model) {
 		// model.addAllAttributes("name",name,"userID",userID);
 		  return "Paid";
 	  }
-	
+
 }
 
 
@@ -87,53 +87,53 @@ public class MypageController {
 
 public class MypageController {
 
-	
+
 	 @GetMapping(value ="/mypage")
 	  public String mypage(Model model) {
 
 	    return "Mypage";
-	  } 
-		 
+	  }
+
 	  @PostMapping(path="/mypage", params = "editbutton")
 	  public String postEdit() {
 		  return "edit";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "attendance-button")
 	  public String postAttendance() {
 		  return "attendance";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "leavingwork-button")
 	  public String postLeavingwork() {
 		  return "leavingwork";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "create-button")
 	  public String postCreate() {
 		  return "create";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "create2-button")
 	  public String postCreate2() {
 		  return "create2";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "expences-button")
 	  public String postExpences() {
 		  return "expences";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "expanceslist-button")
 	  public String postExpanceslist() {
 		  return "expenceslist";
 	  }
-	  
+
 	  @PostMapping(path="/mypage", params = "paidlist-button")
 	  public String postPaidlist() {
 		  return "Paid";
 	  }
-	
+
 }
 
 **/
