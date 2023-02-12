532ed93cfabacb146ac672bc48f34a3623bfe8d9
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-02-12 22:06:30 +0900

    経費（リポジトリ）狩野

--- src/main/java/com/example/demo/repository/PrefecturesRepository.java
+++ src/main/java/com/example/demo/repository/PrefecturesRepository.java
@@ -3,9 +3,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Prefectures;
-/**
- * 都道府県マスタ Repository
- */
+
+
 @Repository
 public interface PrefecturesRepository extends JpaRepository<Prefectures, Long> {
 }
