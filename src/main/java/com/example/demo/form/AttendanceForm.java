748746fc9a9944a36adf67db18df69062b5d63c8
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2023-02-16 01:11:58 +0900

    attendance 出勤報告（廣瀬）

--- src/main/java/com/example/demo/form/AttendanceForm.java
+++ src/main/java/com/example/demo/form/AttendanceForm.java
@@ -4,8 +4,6 @@ package com.example.demo.form;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,31 +13,31 @@ import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
-@Table(name = "attendance")
+//@Table(name = "attendance")
 public class AttendanceForm{
 
 	//@NotNull　無くて良い
-	@Column(name = "user_id")
+	//@Column(name = "user_id")
 	private Integer userId;
 
 
 	@NotEmpty(message="出勤状況を選択して下さい")
-	@Column(name = "status")
+	//@Column(name = "status")
 	private  String status;
 
 	@NotNull(message="出勤日を選択してください")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@Column(name = "start_date")
+	//@Column(name = "start_date")
 	private  Date startdate;
 
 	@NotEmpty(message="出勤時間を選択してください")
 	@DateTimeFormat(pattern="HH:mm:ss")
-	@Column(name = "start_time")
+	//@Column(name = "start_time")
 	//@NotNull
 	private  String starttime;
 
 	//@NotNull
-	@Column(name = "remarks")
+	//@Column(name = "remarks")
 	@Size(max = 200, message = "備考は200文字以内で入力して下さい")
 	private  String remarks;
 
